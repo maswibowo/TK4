1bce5d9ffc72518f31ea11061687f237a9da98a8
maswibowo <157133494+maswibowo@users.noreply.github.com>
2024-01-22 07:54:58 +0700

    Update TestBonusCalculator.java

--- TestBonusCalculator.java
+++ TestBonusCalculator.java
@@ -1,7 +1,11 @@
 public class TestBonusCalculator {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        calculateYearlyBonuses();
+        calculateQuarterlyBonuses();
+    }
 
+    private static void calculateYearlyBonuses() {
         YearlyBonusVisitor yearlyBonusCalculator = new YearlyBonusVisitor();
 
         SalesTrainee bradTrainee = new SalesTrainee(5, 1, 20000);
@@ -13,19 +17,19 @@ public class TestBonusCalculator {
         System.out.println(bradTrainee.accept(yearlyBonusCalculator));
         System.out.println(tomSalesman.accept(yearlyBonusCalculator));
         System.out.println(rossBoss.accept(yearlyBonusCalculator));
+    }
 
+    private static void calculateQuarterlyBonuses() {
         QuarterlyBonusVisitor quarterlyBonusCalculator = new QuarterlyBonusVisitor();
 
-        bradTrainee = new SalesTrainee(1, 0, 20000);
-        tomSalesman = new Salesman(30000, 22);
-        rossBoss = new Boss(200000, 300, 11000);
+        SalesTrainee bradTrainee = new SalesTrainee(1, 0, 20000);
+        Salesman tomSalesman = new Salesman(30000, 22);
+        Boss rossBoss = new Boss(200000, 300, 11000);
 
         System.out.println("\nQUARTERLY BONUS");
 
         System.out.println(bradTrainee.accept(quarterlyBonusCalculator));
         System.out.println(tomSalesman.accept(quarterlyBonusCalculator));
         System.out.println(rossBoss.accept(quarterlyBonusCalculator));
-
     }
-
 }
